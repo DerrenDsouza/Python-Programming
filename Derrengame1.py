bc3e5dfd5de2cc431db0a84605db428b57685449
DerrenDsouza <48007244+DerrenDsouza@users.noreply.github.com>
2019-02-28 14:54:26 +0530

    Add files via upload

--- Derrengame1.py
+++ Derrengame1.py
@@ -3,6 +3,7 @@ user=0
 comp=0
 ide=0
 l=["r","p","s"]
+d={'r':'Rock','p':'Paper','s':'Scissor'}
 while True:
 	u=input("Enter your Choice wheather rock=r, paper=p , scissor=s if not press n to Discontinue")
 	if u=='n':
@@ -12,8 +13,8 @@ while True:
 		print("Intermediate:",ide)
 		exit()
 	c=random.choice(l)
-	print("Computr choses",c)
-	print("User choses",u)
+	print("Computr choses",d[c])
+	print("User choses",d[u])
 	if u==c:
 		print("Tie")
 		ide=ide+1
