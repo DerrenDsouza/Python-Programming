73031c0c40168932a06c1a33a2e7fae8b3c1d16d
DerrenDsouza <48007244+DerrenDsouza@users.noreply.github.com>
2019-02-28 14:39:37 +0530

    Add files via upload

--- Derrengame1.py
+++ Derrengame1.py
@@ -1,23 +1,37 @@
 import random
+user=0
+comp=0
+ide=0
 l=["r","p","s"]
 while True:
-	u=input("Enter your Choice,Press n to Discontinue")
+	u=input("Enter your Choice wheather rock=r, paper=p , scissor=s if not press n to Discontinue")
 	if u=='n':
 		print("Game Over")
+		print("User score is:",user)
+		print("Computer score is:",comp)
+		print("Intermediate:",ide)
 		exit()
 	c=random.choice(l)
 	print("Computr choses",c)
+	print("User choses",u)
 	if u==c:
 		print("Tie")
+		ide=ide+1
 	elif u=="r" and c=="p":
 		print("Computer wins")
+		comp=comp+1
 	elif u=="r" and c=="s":
 		print("You won")
+		user=user+1
 	elif u=="s" and c=="r":
 		print("Computer wins")
+		comp=comp+1
 	elif u=="s" and c=="p":
 		print("You won")
+		user=user+1
 	elif u=="p" and c=="s":
 		print("Computer wins")
+		comp=comp+1
 	elif u=="p" and c=="r":
 		print("You won")
+		user=user+1
